ae2b1d61b99d30c564e5e451da7cf52c99519716
Atharva J <caplock221b@gmail.com>
2022-01-29 18:52:20 +0530

    adding custom directives

--- src/main.js
+++ src/main.js
@@ -5,8 +5,34 @@ import VueResource from "vue-resource";
 // Create eventBus
 // export const eventBus = new Vue();
 
-// Use VueResource
+// Use VueResource for HTTP request
 Vue.use(VueResource);
+
+// Custom Directive
+Vue.directive("rainbow", {
+  bind(el, binding, vnode) {
+    el.style.color =
+      "#" +
+      Math.random()
+        .toString()
+        .slice(2, 8);
+  }
+});
+
+Vue.directive("theme", {
+  bind(el, binding, vnode) {
+    if (binding.value === "wide") {
+      el.style.maxWidth = "1200px";
+    } else if (binding.value === "narrow") {
+      el.style.maxWidth = "560px";
+    }
+
+    if (binding.arg === "column") {
+      el.style.background = "#ddd";
+      el.style.padding = "20px";
+    }
+  }
+});
 
 new Vue({
   el: "#app",
