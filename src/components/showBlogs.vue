067a98906227a31519476570928290b679ab88f9
Atharva J <caplock221b@gmail.com>
2022-01-29 18:38:19 +0530

    GET request and show blogs compoennt

---
+++ src/components/showBlogs.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="show-blogs">
+    <h1>All Blog Articles</h1>
+    <div v-for="blog in blogs" :key="blog.id" class="single-blog">
+      <h2>{{ blog.title }}</h2>
+      <article>{{ blog.body }}</article>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      blogs: [],
+    };
+  },
+  methods: {},
+  created() {
+    this.$http
+      .get("http://jsonplaceholder.typicode.com/posts")
+      .then((res) => {
+        this.blogs = res.data.slice(0, 10);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  },
+};
+</script>
+<style>
+#show-blogs {
+  max-width: 800px;
+  margin: 0 auto;
+}
+
+.single-blog {
+  padding: 20px;
+  margin: 20px 0;
+  box-shadow: border-box;
+  background: #eee;
+}
+</style>
