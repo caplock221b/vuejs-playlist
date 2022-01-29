ced16032522d1c243046701927c2dfc8243233a3
Atharva J <caplock221b@gmail.com>
2022-01-29 15:41:29 +0530

    input binding

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,26 @@
 <template>
   <div>
-    <!-- Helps keep data intact when switching between components  -->
-    <keep-alive>
-      <component v-bind:is="component" />
-    </keep-alive>
-    <button @click="component = 'form-one'">Show form one</button>
-    <button @click="component = 'form-two'">Show form two</button>
+    <add-blog></add-blog>
   </div>
 </template>
 
 <script>
-import formOne from "./components/formOne.vue";
-import formTwo from "./components/formTwo.vue";
+import addBlog from "./components/addBlog.vue";
 
 export default {
   components: {
-    "form-one": formOne,
-    "form-two": formTwo,
+    "add-blog": addBlog,
   },
   data() {
-    return {
-      component: "form-two",
-    };
+    return {};
   },
   methods: {},
 };
 </script>
 
 <style>
-*,
-*::before,
-*::after {
+body {
   margin: 0;
-  padding: 0;
-  box-sizing: border-box;
+  font-family: "Nunito SemiBold";
 }
 </style>
